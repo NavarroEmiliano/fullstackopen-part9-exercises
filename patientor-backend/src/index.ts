e95d807fce17bf503cd5116aab51581e03b5b874
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-06-21 03:59:54 -0300

    exercise 9.9.

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+const PORT = 3001;
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
 
 app.get('/api/ping', (_req,res)=>{
   res.send('pong');
