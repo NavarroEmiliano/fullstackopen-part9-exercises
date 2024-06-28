a77fc039a68b7f962733171334f42889fde9215d
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-06-28 19:20:22 -0300

    total

--- part9-react-typescript/src/App.tsx
+++ part9-react-typescript/src/App.tsx
@@ -1,5 +1,6 @@
 import Content from './components/Content'
 import Header from './components/Header'
+import Total from './components/Total'
 
 function App() {
   const courseName = 'Half Stack application development'
@@ -19,7 +20,7 @@ function App() {
     <div>
       <Header name={courseName} />
       <Content courseParts={courseParts}/>
-      <p>Number of exercises {totalExercises}</p>
+      <Total totalExercises={totalExercises}/>
     </div>
   )
 }
---
+++ part9-react-typescript/src/components/Total.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  totalExercises: number
+}
+
+const Total = (props: Props) => {
+  return <div>{props.totalExercises}</div>
+}
+
+export default Total
