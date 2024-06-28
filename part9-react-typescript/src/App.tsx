c41ea2c60c01f4777336e0199828d0ceb7f62128
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-06-28 19:34:36 -0300

    types

--- part9-react-typescript/src/App.tsx
+++ part9-react-typescript/src/App.tsx
@@ -2,31 +2,59 @@ import Content from './components/Content'
 import Header from './components/Header'
 import Total from './components/Total'
 
-function App() {
+interface coursePartBasic {
+  name: string
+  exerciseCount: number
+  description: string
+  kind: 'basic'
+}
+
+interface CoursePartGroup {
+  name: string
+  exerciseCount: number
+  groupProjectCount: number
+  kind: 'group'
+}
+
+interface CoursePartBackground {
+  name: string
+  exerciseCount: number
+  description: string
+  backgroundMaterial: string
+  kind: 'background'
+}
+
+type CoursePart = coursePartBasic | CoursePartGroup | CoursePartBackground
+
+const App = () => {
   const courseName = 'Half Stack application development'
 
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
-      description: 'This is an awesome course part'
+      description: 'This is an awesome course part',
+      kind: 'basic'
     },
     {
       name: 'Using props to pass data',
       exerciseCount: 7,
-      groupProjectCount: 3
+      groupProjectCount: 3,
+      kind: 'group'
     },
     {
       name: 'Basics of type Narrowing',
       exerciseCount: 7,
-      description: 'How to go from unknown to string'
+      description: 'How to go from unknown to string',
+      kind: 'basic'
     },
     {
       name: 'Deeper type usage',
       exerciseCount: 14,
       description: 'Confusing description',
       backgroundMaterial:
-        'http://type-level-typescript.com/template-literal-types'
+        'http://type-level-typescript.com/template-literal-types',
+      kind: 'background'
     }
   ]
 
